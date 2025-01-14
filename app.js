c533a6a6add40d06aacc4c14fc0d4eb0fba9c47a
Krits-devops <kritagyarm9@gmail.com>
2025-01-13 22:30:26 -0500

    Added real-time location sharing
    Added real-time location sharing using Socket.IO and Leaflet

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 
+
 const server = http.createServer(app);
 const io = socketio(server);
 
+
+io.on("connection", (socket) => {
+    socket.on("send-location", (data)=>{
+        io.emit("receive-location",{id: socket.id, ...data});
+    }) 
+    socket.on("disconnect", () =>{
+        io.emit('user-disconnect', {id: socket.id});
+    })
+})
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 })
 
 app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(3000, () => {
 console.log('Server is running on port 3000');
